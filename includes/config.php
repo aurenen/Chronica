bfe3b13c70d6547247c85c0526e3946afe322f0f
aurenen <aurenen@users.noreply.github.com>
2016-05-08 18:36:49 -0700

    moved to includes/ folder

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+/************************************************************************
+ * file: config.php
+ *
+ * modify the values below with your database information
+ * keep the values within single quotes
+ *
+ * the hostname is usually 'localhost' only change if your host has a 
+ * different setting
+ ************************************************************************/
+
+$db_user = 'username';
+
+$db_pass = 'password';
+
+$db_name = 'chronica';
+
+$db_host = 'localhost';
---
+++ includes/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../config.php';
+
+function db_connect() {
+    $dsn = 'mysql:dbname=' . $db_name . ';host=' . $db_host;
+
+    try {
+        $link = new PDO($dsn, $db_user, $db_pass);
+    }
+    catch (PDOException $ex) {
+        echo 'Connection failed: ' . $ex->getMessage();
+    }
+}
