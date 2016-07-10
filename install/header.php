5713f22e69ed7c6d3008ab7cdff564174bf27016
aurenen <aurenen@users.noreply.github.com>
2016-07-10 01:25:28 -0700

    separated header and footer to keep it dry and have dynamic nav

---
+++ install/header.php
@@ -0,0 +1,45 @@
+<?php
+function is_current($title) {
+    $url = $_SERVER['REQUEST_URI'];
+
+    if (strlen($_SERVER['QUERY_STRING']) > 0) 
+        $page = (strpos($url, "=") > 0) ? (substr($url, strpos($url, "=") + 1)) : (substr($url, strpos($url, "?") + 1));
+    
+    else {
+        preg_match('~\/(.*?)\.php~', $url, $output);
+        $page = $output[1];
+        while (strpos($page, "/") !== false)
+            $page = substr($page, strpos($page, "/") + 1);
+    }
+
+    if ($page == null || strlen($page) == 0) {
+        $page = "index";
+    }
+
+    if ($page === $title) {
+        echo "&rarr; ";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Chronica Installation</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/normalize.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<h1 class="title center">Chronica Installation</h1>
+<div id="wrap">
+    <div id="nav">
+        <h3>Navigation</h3>
+        <a href="index.php"><?php is_current("index"); ?>Start</a>
+        <a href="create.php"><?php is_current("create"); ?>Install</a>
+        <a href="upgrade.php"><?php is_current("upgrade"); ?>Upgrade</a>
+    </div>
+    <div id="content">
---
+++ install/index.php
@@ -0,0 +1,21 @@
+<?php include_once 'header.php'; ?>
+
+        <h2>Introduction</h2>
+        <p>
+        Welcome to the installation starter page. Make sure you have at <strong>PHP 5.4</strong>  or newer, and <strong>MySQL 5.5</strong> or newer installed on your server first. If your PHP or MySQL is an older version, this might still work, but there's no way to guarantee it.
+        </p>
+        <p>
+        After you successfully run the installer, please delete or remove read access (chmod 000) to the <strong>/install/</strong> directory to be extra safe.
+        </p>
+        <h3>Instructions</h3>
+        <p>
+        Use the browser's back and forward nagivation with caution, and please avoid reloading after you submit any forms. 
+        </p>
+        <p>
+        If you run into any issues installing, you can ask for support at the <a href="http://board.aurenen.org">forums</a>.
+        </p>
+        <p>
+        To begin, click on the "Install" link to the left.
+        </p>
+
+<?php include_once 'footer.php'; ?>
