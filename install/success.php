03aa2600a1d8d6e0123df04c480ceb94385ba3d2
aurenen <aurenen@users.noreply.github.com>
2016-07-10 01:32:49 -0700

    init

---
+++ install/success.php
@@ -0,0 +1,8 @@
+<?php include_once 'header.php'; ?>
+
+        <h2>Success!</h2>
+
+        <div class="warning">Remove or CHMOD 000 the /install/ folder to be extra safe.</div>
+        <div class="success">Please <a href="../login.php">log in</a> with your username and password.</div>
+
+<?php include_once 'footer.php'; ?>
