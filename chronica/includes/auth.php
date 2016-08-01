c60894b0a0dccae84bb360c6fba576f41aaaa75d
aurenen <aurenen@users.noreply.github.com>
2016-08-01 00:44:37 -0700

    Verify user/pass with settings in database

--- chronica/includes/auth.php
+++ chronica/includes/auth.php
@@ -11,29 +11,38 @@
  ************************************************************/
 
 /**
- * [verifyUser description]
- * @param  [type] $user [description]
- * @param  [type] $pass [description]
- * @return [type]       [description]
+ * Checks parameters with database values to verify if login matches
+ * @param  string $user
+ * @param  string $pass 
+ * @return booloan 
  */
 function verifyUser($user, $pass) {
     $db = db_connect();
+    $verify = false;
 
-    // check password length, bcrypt only uses the first 72 characters
-    if (strlen($pass) > 72) { 
-        header('Location: login.php?error');
-        exit();
+    $sql = "SELECT u.`set_value` AS user, p.`set_value` AS pass FROM  
+        (SELECT `set_value` FROM `settings` WHERE `set_key` = 'username') u,
+        (SELECT `set_value` FROM `settings` WHERE `set_key` = 'password') p";
+
+    $stmt = $db->prepare($sql);
+
+    try {
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // hash password before checking with db
+        $hasher = new PasswordHash(8, FALSE);
+        // if match, result[user] == 'username' and result[pass] == 'password'
+        if (strcmp($user, $result['user']) === 0 && $hasher->CheckPassword($pass, $result['pass'])) {
+            $verify = true;
+        }
+        unset($hasher);
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage();
+        $verify = false;
     }
 
-    // hash password before checking with db
-    $hasher = new PasswordHash(8, FALSE);
-
-    // if match, return result[0][0] == 'username' and result[1][0] == 'password'
-    $sql = "SELECT `set_key` FROM `settings` 
-        WHERE (`set_key` = 'username' AND `set_value` = :user)
-        UNION 
-        SELECT `set_key` FROM `settings` 
-        WHERE (`set_key` = 'password' AND `set_value` = :pass";
-
     $db = null;
+    return $verify;
 }
