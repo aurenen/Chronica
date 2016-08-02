faa55d0d4b12d9b464a2ab1e82eda78c7021f4c6
aurenen <aurenen@users.noreply.github.com>
2016-08-01 21:43:26 -0700

    Keeping things DRY, moved is_current() from install/header.php to here and func to print credit link

--- chronica/includes/util.php
+++ chronica/includes/util.php
@@ -10,6 +10,52 @@
  * 
  ************************************************************/
 
+/**
+ * Prints credit text/link.
+ * @param  string $type values [url|full|link]
+ * @return void
+ */
+function poweredBy($type = 'full') {
+    $url = 'https://github.com/aurenen/chronica';
+    if ($type == 'url')
+        echo $url;
+    else if ($type == 'full')
+        echo 'Powered by <a href="'.$url.'">Chronica</a>';
+    else if ($type == 'link')
+        echo '<a href="'.$url.'">Chronica</a>';
+    else
+        echo 'Chronica';
+}
+
+/**
+ * Check if page is current url for navigation
+ * http://blog.aurenen.org/2015/11/php-dynamic-navigationpage-title/
+ * 
+ * @param  string $title : filename without extension
+ * @return boolean 
+ */
+function is_current($title) {
+    $url = $_SERVER['REQUEST_URI'];
+
+    if (strlen($_SERVER['QUERY_STRING']) > 0) 
+        $page = (strpos($url, "=") > 0) ? (substr($url, strpos($url, "=") + 1)) : (substr($url, strpos($url, "?") + 1));
+    
+    else {
+        preg_match('~\/(.*?)\.php~', $url, $output);
+        $page = $output[1];
+        while (strpos($page, "/") !== false)
+            $page = substr($page, strpos($page, "/") + 1);
+    }
+
+    if ($page == null || strlen($page) == 0) {
+        $page = "index";
+    }
+
+    if ($page === $title) {
+        echo "&rarr; ";
+    }
+}
+
 // http://webcheatsheet.com/php/get_current_page_url.php
 function curPageURL() {
     $pageURL = 'http';
