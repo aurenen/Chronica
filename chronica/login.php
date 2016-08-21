0b9c91970a50915ba95b39cc5f3d1a39b3648ffb
aurenen <aurenen@users.noreply.github.com>
2016-08-21 02:19:40 -0700

    Implemented login verification

--- chronica/login.php
+++ chronica/login.php
@@ -15,9 +15,32 @@ require_once 'includes/util.php';
 require_once 'includes/auth.php';
 require_once 'includes/PasswordHash.php';
 
+session_start();
+
+if (isset($_POST['login'])) {
+    if (verifyUser($_POST['username'], $_POST['password'])) {
+        $_SESSION['login'] = md5($_POST['username']);
+        header('Location: dash.php');
+        exit();
+    }
+}
 require_once 'includes/admin_header.php'; 
 ?>
 
+        <form action="login.php" method="post">
+            <div class="form-row">
+                <label>Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="form-row">
+                <label>Passcode</label>
+                <input type="password" name="password">
+            </div>
+            <div class="form-row">
+                <label>&nbsp;</label>
+                <input type="submit" name="login" value="Login">
+            </div>
+        </form>
 
 <?php 
 require_once 'includes/admin_header.php'; 
