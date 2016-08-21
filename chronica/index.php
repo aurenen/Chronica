a3a9699ee46aca1d054d3fd9fd0c9fa085438b65
aurenen <aurenen@users.noreply.github.com>
2016-08-20 23:54:19 -0700

    derp

---
+++ chronica/index.php
@@ -0,0 +1,21 @@
+<?php 
+/************************************************************
+ *
+ *  (c) Chronica
+ *  https://github.com/aurenen/chronica
+ *  
+ *  index.php
+ *  
+ *  Handles the display of entries. View may change if admin
+ *  is logged in.
+ * 
+ ************************************************************/
+
+require_once 'includes/connect.php';
+require_once 'includes/util.php';
+
+include_once 'template/header.php'; 
+?>
+
+
+<?php include_once 'template/footer.php'; ?>
---
+++ chronica/login.php
@@ -0,0 +1,24 @@
+<?php 
+/************************************************************
+ *
+ *  (c) Chronica
+ *  https://github.com/aurenen/chronica
+ *  
+ *  login.php
+ *  
+ *  Handles login verification for admin
+ * 
+ ************************************************************/
+
+require_once 'includes/connect.php';
+require_once 'includes/util.php';
+require_once 'includes/auth.php';
+require_once 'includes/PasswordHash.php';
+
+require_once 'includes/admin_header.php'; 
+?>
+
+
+<?php 
+require_once 'includes/admin_header.php'; 
+?>
