c72fbab3b3b86b0ac964bcfc9b1ef1867cf2bb42
aurenen <aurenen@users.noreply.github.com>
2016-08-22 00:02:16 -0700

    Added markdown editor

--- chronica/includes/admin_footer.php
+++ chronica/includes/admin_footer.php
@@ -3,5 +3,21 @@
 <div id="footer">
     <p>&copy; <?php poweredBy('link'); ?> 2016.</p>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/6.0.2/markdown-it.min.js"></script>
+<script>
+    var md = window.markdownit();  
+    function generate() {
+      var result = md.render($("#md").val());
+      $("#preview,#html").html(result);
+    }
+    $(document).ready(function(){
+      generate();
+      $("#md").on("keyup", function (){
+        generate();
+      });
+    });
+</script>
 </body>
 </html>
