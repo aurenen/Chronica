fe23aa5c774b83afc1c6996b9b60b2295622a901
aurenen <aurenen@users.noreply.github.com>
2016-09-07 23:36:57 -0700

    Changed error messages to nicer style

--- chronica/category.php
+++ chronica/category.php
@@ -23,28 +23,27 @@ if (!isset($_SESSION['login'])) {
 } 
 
 $cats = getCategories();
-
+$add_msg = '';
 $add = true;
 $added = false;
+
 if ( isset($_POST['add_cat']) ) {
     $_SESSION['cat_name'] = htmlentities($_POST['cat_name']);
     $_SESSION['cat_perma'] = htmlentities($_POST['cat_perma']);
     $_SESSION['cat_desc'] = htmlentities($_POST['cat_desc']);
 
-    $add_msg = '<div class="warning">';
     if ( strlen($_POST['cat_name']) < 2 || strlen($_POST['cat_name']) > 25 || preg_match('/[^A-Za-z0-9\.\&\!\s]+/', $_POST['cat_name']) === 1) {
-        $add_msg .= 'Category name must be greater than 1 and less than 25 characters and contain no symbols. ';
+        $add_msg .= '<div class="warning">Category name must be greater than 1 and less than 25 characters and contain no symbols.</div>';
         $add = false;
     }
     if ( strlen($_POST['cat_perma']) < 2 || strlen($_POST['cat_perma']) > 25 || preg_match('/[^a-z0-9\-]+/', $_POST['cat_perma']) === 1) {
-        $add_msg .= 'Category permalink must be greater than 1 and less than 25 characters, all lower case, and contain no symbols. ';
+        $add_msg .= '<div class="warning">Category permalink must be greater than 1 and less than 25 characters, all lower case, and contain no symbols.</div>';
         $add = false;
     }
     if ( strlen($_POST['cat_desc']) > 200) {
-        $add_msg .= 'Category description must be less than 200 characters. ';
+        $add_msg .= '<div class="warning">Category description must be less than 200 characters.</div>';
         $add = false;
     }
-    $add_msg .= '</div>';
 
     if ($add) {
         $added = addCategory($_POST['cat_name'], $_POST['cat_perma'], $_POST['cat_desc']);
