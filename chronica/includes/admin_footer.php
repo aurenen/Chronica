515f5df2943d6c6d5b497e5b3bc6cb6b9f4c5eb1
aurenen <aurenen@users.noreply.github.com>
2017-02-18 01:46:03 -0800

    Removed Markdown-it

--- chronica/includes/admin_footer.php
+++ chronica/includes/admin_footer.php
@@ -6,19 +6,9 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="js/flatpickr.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/6.0.2/markdown-it.min.js"></script>
 <script src="js/simplemde.min.js"></script>
 <script>
-    /*var md = window.markdownit();  
-    function generate() {
-      var result = md.render($("#md").val());
-      $("#preview,#html").html(result);
-    }*/
     $(document).ready(function(){
-      /*generate();
-      $("#md").on("keyup", function (){
-        generate();
-      });*/
       $(".date").flatpickr();
     });
 
