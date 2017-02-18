e0a23e5e0a71dc43eaa1457e5f4172e323e98963
aurenen <aurenen@users.noreply.github.com>
2017-02-18 01:38:03 -0800

    Removed emulation false and added charset

--- chronica/includes/connect.php
+++ chronica/includes/connect.php
@@ -17,12 +17,12 @@
 function db_connect() {
     require_once 'config.php';
 
-    $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name;
+    $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
 
     try {
         $link = new PDO($dsn, $db_user, $db_pass);
         $link->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
-        $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        //$link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $link->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
 
